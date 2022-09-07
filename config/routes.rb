28878d327d0b7bd760455b92b948d9f7bdb380a3
tamaki56 <http56@icloud.com>
2022-09-07 16:01:59 +0000

    add homes controller,views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
+  get 'homes/about' => 'homes#about'
+  get 'homes/index' => 'homes#index'
+  get 'homes/news' => 'homes#news'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
